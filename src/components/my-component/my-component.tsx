472355c6ffb4a793c952b099d1b9dd84dafd09eb
Jack Sloan <hi@jacksloan.dev>
2022-06-09 20:57:20 -0500

    typography is working now but not color attributes on buttons???

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -7,6 +7,11 @@ import { Component, h } from '@stencil/core';
 })
 export class MyComponent {
   render() {
-    return <ion-button>My Button</ion-button>;
+    return [
+    <ion-button>No Color</ion-button>,
+    <ion-button color="primary">Primary</ion-button>,
+    <ion-button color="secondary">Primary</ion-button>,
+    <ion-button color="warning">Primary</ion-button>,
+  ];
   }
 }
