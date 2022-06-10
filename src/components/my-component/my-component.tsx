44372a17107e1e80c15c10daadf3d9837daf5777
Jack Sloan <hi@jacksloan.dev>
2022-06-10 12:04:48 -0500

    add working example
    in order for ionic styles to penetrate the nested shadow root when wrapping an ionic component in a custom element, we have to link to the ionic core styles
    
    an offline alternative would be to create a <style> element with the required styles as innerHTML and append that

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, Element, h, Host } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
@@ -6,12 +6,29 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class MyComponent {
+  @Element() el: HTMLElement;
+
+  constructor() {
+    // required for ionic styles to apply to nested shadow roots,
+    const l = document.createElement('link');
+    l.setAttribute('rel', 'stylesheet');
+    l.setAttribute('href', 'https://cdn.jsdelivr.net/npm/@ionic/core/css/core.css');
+    this.el.shadowRoot.prepend(l);
+  }
+
   render() {
-    return [
-    <ion-button>No Color</ion-button>,
-    <ion-button color="primary">Primary</ion-button>,
-    <ion-button color="secondary">Primary</ion-button>,
-    <ion-button color="warning">Primary</ion-button>,
-  ];
+    return (
+      <Host>
+        <ion-button color="primary">Primary</ion-button>
+        <ion-button color="secondary">Secondary</ion-button>
+        <ion-button color="tertiary">Tertiary</ion-button>
+        <ion-button color="success">Success</ion-button>
+        <ion-button color="warning">Warning</ion-button>
+        <ion-button color="danger">Danger</ion-button>
+        <ion-button color="light">Light</ion-button>
+        <ion-button color="medium">Medium</ion-button>
+        <ion-button color="dark">Dark</ion-button>
+      </Host>
+    );
   }
 }
